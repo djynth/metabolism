8088a7f0f929759d86831b4ef5d982acaf09acb3
Dominic Zirbel <zirbinator@gmail.com>
2013-07-23 22:43:57 -0400

    Replaced tabs with accordians. The accordians are fully functional and no other functionality is broken; however, there is an occassional bug where the content of the accordian is hidden until it expands all the way.

--- js/organs.js
+++ js/organs.js
@@ -1,15 +1,16 @@
 var ORGAN_FADE_IN  = 450;
 var ORGAN_FADE_OUT = 150;
+var ORGAN_TRANSITION = ORGAN_FADE_OUT + ORGAN_FADE_IN;
 
 $(document).ready(function() {
-    selectOrgan(parseInt($('.tab-holder .organ-title').first().attr('value')));
+    selectOrgan(parseInt($('.accordian-header').first().attr('value')), true);
 
-    $('.organ-title').click(function() {
+    $('.accordian-header').click(function() {
         selectOrgan($(this).attr('value'));
     });
 });
 
-function selectOrgan(organ)
+function selectOrgan(organ, firstTime)
 {
     $('#organ-visual').find('img').fadeOut(ORGAN_FADE_OUT, function() {
         $(this).remove();
@@ -27,25 +28,29 @@ function selectOrgan(organ)
         organImage.fadeIn(ORGAN_FADE_IN);
     }, ORGAN_FADE_OUT);
 
-    $('.organ-title').each(function() {
-        if ($(this).hasClass('global-title')) {
-            return;
-        }
+    $('.accordian-content').each(function() {
         var tabOrgan = $(this).attr('value');
+        var height = $(this).hasClass('pathway-holder') ? getPathwayContentHeight() : getResourceContentHeight();
         if (tabOrgan == organ) {                    // select this tab
             $(this).addClass('active');
-            $(this).children('.cover').show();
-            $('.pathway-holder[value="'  + tabOrgan + '"]').show();
-            $('.resource-holder[value="' + tabOrgan + '"]').show();
+            if (firstTime) {
+                $(this).css('height', height);
+                $(this).mCustomScrollbar('update');
+            } else {
+                $(this).animate({ height: height }, ORGAN_TRANSITION, function() {
+                    $(this).mCustomScrollbar('update');
+                });
+            }
+            
         } else {                                    // unselect this tab
             $(this).removeClass('active');
-            $(this).children('.cover').hide();
-            $('.pathway-holder[value="'  + tabOrgan + '"]').hide();
-            $('.resource-holder[value="' + tabOrgan + '"]').hide();
+            if (firstTime) {
+                $(this).css('height', 0);
+            } else {
+                $(this).animate({ height: 0 }, ORGAN_TRANSITION);
+            }
         }
     });
-
-    updateScrollbars();
 
     $.ajax({
         url: 'index.php?r=site/organColor',
