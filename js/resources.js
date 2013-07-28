6e0d7c5dbe1209a114c62a22430a240488bf9a6b
Dominic Zirbel <zirbinator@gmail.com>
2013-07-27 21:04:42 -0400

    Resource visualization cleanups.

--- js/resources.js
+++ js/resources.js
@@ -4,25 +4,33 @@ var COLOR_DECREASE = "232,12,15";
 $(document).ready(function() {
     refreshResources();
 
-    $('.resource-data').mouseenter(function() {
-        var resource = $(this).attr('value');
-
-        $('#resource-visual').append('<img name="' + resource + '" src="' + baseUrl + 'img/molecules/' + resource + '"'
-            + 'alt="' + resource + '" class="image-content hidden">');
-
-        setTimeout(function() {
-            $('#resource-visual img[name="' + resource + '"]').fadeIn(250);
-        }, 300);
-    });
-
-    $('.resource-data').mouseleave(function() {
-        var resource = $(this).attr('value');
-
-        $('#resource-visual img[name="' + resource + '"]').fadeOut(100, function() {
-            $(this).remove();
-        });
+    $('.resource-data').hover(function() {
+        visualizeResource($(this).attr('value'), true);
+    }, function() {
+        visualizeResource($(this).attr('value'), false);
     });
 });
+
+function visualizeResource(resource, show)
+{
+    if (show) {
+        var visualization = $('<img>')
+            .attr('name', resource)
+            .attr('alt', resource)
+            .attr('src', baseUrl + 'img/molecules/' + resource)
+            .addClass('image-content hidden');
+
+        $('#resource-visual').append(visualization);
+
+        setTimeout(function() {
+            visualization.fadeIn(300);
+        }, 350);
+    } else {
+        $('#resource-visual img[name="' + resource + '"]').fadeOut(150, function() {
+            $(this).remove();
+        });
+    }
+}
 
 function refreshResources(resources)
 {
