887dd694c694403884162e7dfb5eff0aa616eae6
Dominic Zirbel <zirbinator@gmail.com>
2013-07-28 13:15:05 -0400

    Added a basic header, which contains the point and turn counters and a placeholder user account section.

---
+++ protected/views/site/header.php
@@ -0,0 +1,9 @@
+<div id="header">
+    <p id="points">0 Points</p>
+    <p id="turns" >50/50 Turns</p>
+
+    <div id="account">
+        <p class="login-text">Not logged in</p>
+        <i class="icon-user icon-white dropdown-toggle" data-toggle="dropdown" href="#"></i>
+    </div>
+</div>
