c550f6f78a2b2a819233c67508804420fb698d35
Dominic Zirbel <zirbinator@gmail.com>
2013-08-14 23:37:46 -0400

    Primary resources are now hidden in the main resource display areas as they are shown in the resource trackers section.

--- protected/views/site/resource.php
+++ protected/views/site/resource.php
@@ -1,4 +1,4 @@
-<div class="resource-data resource-bar" value="<?= $resource->id ?>" max-shown="<?= $resource->max_shown_value ?>"
+<div class="resource-data resource-bar <?= $resource->primary ? 'primary' : '' ?>" value="<?= $resource->id ?>" max-shown="<?= $resource->max_shown_value ?>"
     init="no" abbr="<?= $resource->abbr ?>" name="<?= $resource->name ?>" full-name="<?= $resource->full_name ?>">
     <div class="progress">
         <span class="resource-name"><?= $resource->name ?></span>
