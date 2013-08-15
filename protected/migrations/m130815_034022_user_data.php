d7a89766c895b6ed70964ca17eb9ca4ccd6169b0
Dominic Zirbel <zirbinator@gmail.com>
2013-08-15 00:08:48 -0400

    Added database tables to track game data, including which pathways were run each turn and the resource levels each turn.

---
+++ protected/migrations/m130815_034022_user_data.php
@@ -0,0 +1,45 @@
+<?php
+
+class m130815_034022_user_data extends CDbMigration
+{
+    public function up()
+    {
+        $this->createTable('games', array(
+            'id' => 'int primary key auto_increment',
+            'completed' => 'tinyint(1) default 0',
+            'score' => 'smallint',
+        ), 'Engine InnoDB');
+
+        $this->createTable('user_games', array(
+            'user_id' => 'int not null',
+            'game_id' => 'int not null',
+        ), 'Engine InnoDB');
+
+        $this->createTable('moves', array(
+            'id' => 'int primary key auto_increment',
+            'game_id' => 'int not null',
+            'move_number' => 'smallint',
+            'pathway_id' => 'smallint',
+        ), 'Engine InnoDB');
+
+        $this->createTable('move_levels', array(
+            'move_id' => 'int',
+            'resource_id' => 'smallint',
+            'resource_level' => 'int',
+        ), 'Engine InnoDB');
+
+        $this->addForeignKey('fk_user_games_user_user_id',           'user_games',  'user_id',     'user',      'id', 'cascade', 'cascade');
+        $this->addForeignKey('fk_user_games_games_game_id',          'user_games',  'game_id',     'games',     'id', 'cascade', 'cascade');
+        $this->addForeignKey('fk_moves_games_game_id',               'moves',       'game_id',     'games',     'id', 'cascade', 'cascade');
+        $this->addForeignKey('fk_move_levels_moves_move_id',         'move_levels', 'move_id',     'moves',     'id', 'cascade', 'cascade');
+        $this->addForeignKey('fk_move_levels_resources_resource_id', 'move_levels', 'resource_id', 'resources', 'id', 'cascade', 'cascade');
+    }
+
+    public function down()
+    {
+        $this->dropTable('move_levels');
+        $this->dropTable('moves');
+        $this->dropTable('user_games');
+        $this->dropTable('games');
+    }
+}
