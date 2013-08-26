4fe7540d7e0074e1c3116b4d3cc5bebb7da971e1
Dominic Zirbel <zirbinator@gmail.com>
2013-08-25 20:21:50 -0400

    Rework of the molecule images, so that they are now named by the resource abbreviation (since resource id is more liable to change). Added molecule images for new resources.

--- protected/views/site/resource-visual.php
+++ protected/views/site/resource-visual.php
@@ -1,5 +1,5 @@
 <div class="resource-visual-content" value="<?= $resource->id ?>">
-    <img class="resource-visual-image" alt="" src="<?= Yii::app()->request->baseUrl . 'img/molecules/' . $resource->id . '.png' ?>">
+    <img class="resource-visual-image" alt="" src="<?= Yii::app()->request->baseUrl . 'img/molecules/' . strtolower($resource->abbr) . '.png' ?>">
     <p class="resource-visual-formula">Formula: <?= $resource->formula ?></p>
     <p class="resource-visual-description"><?= $resource->description ?></p>
 </div>
