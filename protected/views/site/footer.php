a102556c954dccb4c43e141ea22182575fa43da5
Dominic Zirbel <djynth@gmail.com>
2014-05-09 20:37:23 -0400

    cleanups to the resource visual

--- protected/views/site/footer.php
+++ protected/views/site/footer.php
@@ -54,20 +54,20 @@
 
         <table id="trackers">
             <?php foreach ($primary_resources as $resource): ?>
-            <td class="tracker" res-id="<?= $resource->id ?>">
-                <div class="header">
-                    <p class="title"><?= $resource->name ?></p>
-                    <p class="total"><?= $resource->getTotal() ?></p>
-                </div>
+                <td class="tracker res-info-source" res="<?= $resource->id ?>">
+                    <div class="header">
+                        <p class="title"><?= $resource->name ?></p>
+                        <p class="total"><?= $resource->getTotal() ?></p>
+                    </div>
 
-                <?php foreach ($non_global as $organ): ?>
-                <div class="organ" organ-id="<?= $organ->id ?>">
-                    <p class="amount"><?= $resource->getAmount($organ->id) ?></p>
-                    <p class="organ-name"><?= $organ->name ?></p>
-                    <div class="icons"></div>
-                </div>
-                <?php endforeach ?>
-            </td>
+                    <?php foreach ($non_global as $organ): ?>
+                        <div class="organ" organ="<?= $organ->id ?>">
+                            <p class="amount"><?= $resource->getAmount($organ->id) ?></p>
+                            <p class="name"><?= $organ->name ?></p>
+                            <div class="icons"></div>
+                        </div>
+                    <?php endforeach ?>
+                </td>
             <?php endforeach ?>
 
             <td class="tracker actions">
@@ -76,18 +76,37 @@
                 </div>
 
                 <?php foreach ($non_global as $organ): ?>
-                <div class="organ" organ-id="<?= $organ->id ?>">
-                    <?php if ($organ->hasAction()): ?>
-                    <p class="amount"><?= $organ->getActionCount() ?></p>
-                    <p class="organ-name"><?= $organ->action_name ?></p>
-                    <?php endif ?>
-                </div>
+                    <div class="organ" organ="<?= $organ->id ?>">
+                        <?php if ($organ->hasAction()): ?>
+                            <p class="amount"><?= $organ->getActionCount() ?></p>
+                            <p class="name"><?= $organ->action_name ?></p>
+                        <?php endif ?>
+                    </div>
                 <?php endforeach ?>
             </td>
         </table>
 
         <div id="resource-visual">
-            <i class="icon-remove" id="resource-visual-close"></i>
+            <div>
+                <div class="header">
+                    <p class="name">Name</p>
+                    <i class="icon-remove"></i>
+                </div>
+                <div class="content">
+                    <div>
+                        <p class="aliases"></p>
+                        <p class="formula"></p>
+                        <p class="hard_min"></p>
+                        <p class="hard_max"></p>
+                        <p class="soft_min"></p>
+                        <p class="soft_max"></p>
+                        <p class="description"></p>
+                    </div>
+                    <div>
+                        <img class="image" alt="" src="">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
