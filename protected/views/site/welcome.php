3aef327586d7f3ec68388b3ca3e5e61c5d4e18cc
Dominic Zirbel <djynth@gmail.com>
2014-05-25 17:02:21 -0400

    improved the welcome overlay sizing and coloring

--- protected/views/site/welcome.php
+++ protected/views/site/welcome.php
@@ -10,5 +10,36 @@
             <p class="title">Welcome to Metabolism Fun ™</p>
             <i class="icon-remove"></i>
         </div>
+
+        <div class="content">
+            <div class="options">
+                <div class="option">
+                    <label>New Game</label>
+                    <img alt="New Game">
+                </div>
+
+                <div class="option">
+                    <label>Resume Game</label>
+                    <img alt="Resume Game">
+                </div>
+
+                <div class="option">
+                    <label>Tutorial</label>
+                    <img alt="Tutorial">
+                </div>
+
+                <?php if ($user === null): ?>
+                    <div class="option">
+                        <label>Log In</label>
+                        <!-- TODO -->
+                    </div>
+                <?php else: ?>
+                    <div class="option">
+                        <label>Load Game</label>
+                        <!-- TODO -->
+                    </div>
+                <?php endif ?>
+            </div>
+        </div>
     </div>
 </div>
