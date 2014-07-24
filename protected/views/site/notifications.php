af91370a05a2add27bb1bcd815c74093f54280c7
Dominic Zirbel <djynth@gmail.com>
2014-07-24 06:44:38 -0400

    removed unnecessary files for the account dropdown and notification window

--- protected/views/site/notifications.php
+++
@@ -1,8 +0,0 @@
-<div id="notifications">
-    <div class="header">
-        <p>Notifications</p>
-        <i class="fa fa-toggle-up minimize-notify"></i>
-    </div>
-
-    <div class="content"></div>
-</div>
